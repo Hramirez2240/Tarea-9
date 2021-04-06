8a1790e23e15ba922eb794c9a607f5f26de399c3
Hramirez2240 <hramirez2298@gmail.com>
2021-04-06 14:07:04 -0400

    inputs

--- Tarea-9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ Tarea-9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,110 +13,70 @@ namespace Tarea_9.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-<<<<<<< HEAD
-#line 1 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 1 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 1 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 2 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 2 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 2 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 3 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 3 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 3 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 4 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 4 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 4 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 5 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 5 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 5 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 6 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 6 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 6 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 7 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 7 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 7 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 8 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 8 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 8 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Tarea_9;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 9 "C:\2021\Tarea-9\Tarea-9\_Imports.razor"
-=======
-#line 9 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\_Imports.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 9 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\_Imports.razor"
 using Tarea_9.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-<<<<<<< HEAD
-#line 2 "C:\2021\Tarea-9\Tarea-9\Pages\Index.razor"
-=======
-#line 2 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\Pages\Index.razor"
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
+#line 2 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\Pages\Index.razor"
 using Models;
 
 #line default
@@ -131,21 +91,12 @@ using Models;
         }
         #pragma warning restore 1998
 #nullable restore
-<<<<<<< HEAD
-#line 46 "C:\2021\Tarea-9\Tarea-9\Pages\Index.razor"
-      
-    string cedula = "", nombre = "", apellido = "", telefono = "", correo = "",
-        fecha_nacimiento = "", tipo_sangre = "", provincia = "", direccion = "", lat = "", lon = "";
-
-    int covid, mensaje = 0, mensaje_error = 0, mensaje_vacio = 0;
-=======
-#line 53 "C:\Users\User\Desktop\Programacion 3\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 56 "C:\Users\Ramirez Rodriguez\Desktop\Héctor\Itla\Materias\Programación III\Tarea-9\Tarea-9\Pages\Index.razor"
       
     string cedula = "", nombre = "", apellido = "", telefono = "", correo = "",
         fecha_nacimiento = "", tipo_sangre = "", provincia = "", direccion = "", lat = "", lon = "", covid = "";
 
     int mensaje = 0, mensaje_error = 0, mensaje_vacio = 0;
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
     string justificacion = "";
     string correo_verificado;
     bool validar;
@@ -182,20 +133,12 @@ using Models;
                 vacu.Direccion = direccion;
                 vacu.Latitud = lat;
                 vacu.Longitud = lon;
-<<<<<<< HEAD
-                //vacu.LeHaDadoCovid = Convert.ToBoolean(covid);
-                vacu.Justifique = justificacion;
-
-                if (vacu.Cedula == "" || vacu.Nombre == "" || vacu.Apellido == "" || vacu.Teléfono == "" || vacu.CorreoElectronico == "")
-                /*vacu.FechaDeNacimiento == "" || vacu.TipoDeSangre == "" || vacu.Provincia == "" || vacu.Direccion == "" || vacu.Latitud == "" || vacu.Longitud == "" || vacu.Justifique == ""*/
-=======
                 vacu.LeHaDadoCovid = covid;
                 vacu.Justifique = justificacion;
 
                 if (vacu.Cedula == "" || vacu.Nombre == "" || vacu.Apellido == "" || vacu.Teléfono == "" || vacu.CorreoElectronico == "" ||
                     vacu.FechaDeNacimiento == "" || vacu.TipoDeSangre == "" || vacu.Provincia == "" || vacu.Direccion == "" || vacu.Latitud == "" ||
                     vacu.Longitud == "" || vacu.LeHaDadoCovid == "" || vacu.Justifique == "")
->>>>>>> 757a033339ab5219600ed7223377cb3ac303ada9
                 {
                     mensaje = 0;
                     mensaje_error = 0;
