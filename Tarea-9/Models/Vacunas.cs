715fd163b3499c84f2030bf963ff59fc9245a09a
Hramirez2240 <hramirez2298@gmail.com>
2021-04-05 22:26:17 -0400

    Scaffold realizado

---
+++ Tarea-9/Models/Vacunas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Tarea_9.Models
+{
+    public partial class Vacunas
+    {
+        public int Id { get; set; }
+        public string Cedula { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string CorreoElectronico { get; set; }
+        public DateTime? FechaDeNacimiento { get; set; }
+        public string TipoDeSangre { get; set; }
+        public string Provincia { get; set; }
+        public string Direccion { get; set; }
+        public string Latitud { get; set; }
+        public string Longitud { get; set; }
+        public bool? LeHaDadoCovid { get; set; }
+        public string Justifique { get; set; }
+    }
+}
---
+++ Tarea-9/Models/db_a72461_backupContext.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Tarea_9.Models
+{
+    public partial class db_a72461_backupContext : DbContext
+    {
+        public db_a72461_backupContext()
+        {
+        }
+
+        public db_a72461_backupContext(DbContextOptions<db_a72461_backupContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Vacunas> Vacunas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySql("server=mysql5044.site4now.net;uid=a72461_backup;pwd=grupon00;database=db_a72461_backup", x => x.ServerVersion("8.0.21-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Vacunas>(entity =>
+            {
+                entity.ToTable("vacunas");
+
+                entity.Property(e => e.Apellido)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Cedula)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.CorreoElectronico)
+                    .HasColumnName("Correo Electronico")
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Direccion)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.FechaDeNacimiento)
+                    .HasColumnName("Fecha de Nacimiento")
+                    .HasColumnType("date");
+
+                entity.Property(e => e.Justifique)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Latitud)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.LeHaDadoCovid).HasColumnName("Le ha dado covid");
+
+                entity.Property(e => e.Longitud)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.Provincia)
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.TipoDeSangre)
+                    .HasColumnName("Tipo de Sangre")
+                    .HasColumnType("varchar(50)")
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
