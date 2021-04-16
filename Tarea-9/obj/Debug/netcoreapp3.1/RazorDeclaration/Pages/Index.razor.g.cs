90e608e65d4768cf9dbb768489ab5ed8f2f64309
Dhimas Angeles <dhimas15@outlook.com>
2021-04-15 20:13:43 -0400

    klk

--- Tarea-9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ Tarea-9/obj/Debug/netcoreapp3.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,84 +13,84 @@ namespace Tarea_9.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 1 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 2 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 3 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 4 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 5 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 6 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 7 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 8 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Tarea_9;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\_Imports.razor"
+#line 9 "C:\Users\dhima\Tarea-9\Tarea-9\_Imports.razor"
 using Tarea_9.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 2 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
 using Models;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 3 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
 using System.Net.Mail;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 4 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
 using System.Text.RegularExpressions;
 
 #line default
@@ -105,7 +105,7 @@ using System.Text.RegularExpressions;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 77 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 77 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
       
 
     private string Menssage{get;set;} = "";
@@ -205,7 +205,7 @@ using System.Text.RegularExpressions;
 #line hidden
 #nullable disable
 #nullable restore
-#line 170 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 170 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
                                                       
     public static bool IsValidEmail(string strMailAddress)
         {
@@ -219,7 +219,7 @@ using System.Text.RegularExpressions;
 #line hidden
 #nullable disable
 #nullable restore
-#line 177 "C:\Users\One Piece\Desktop\Tarea-9\Tarea-9\Pages\Index.razor"
+#line 177 "C:\Users\dhima\Tarea-9\Tarea-9\Pages\Index.razor"
                                   
     private void SenMail(String enviar1,String mensajeEnviar){
         try{
